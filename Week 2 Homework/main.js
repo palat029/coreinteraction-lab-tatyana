3d9b68ab83fde240b7a69d468a6fd974646fc253
palat029 <TatyanaPalacios@tatyanas-mbp.nyc.rr.com>
2016-02-17 17:53:21 -0500

    Week3

---
+++ Week 2 Homework/main.js
@@ -0,0 +1,19 @@
+<script src="https://code.jquery.com/jquery-2.2.0.min.js">
+</script>
+
+<script>
+//select an element and do something with it from the
+//dollar sign calls out that you are calling out jquery the website  
+	$ ("content tab-2").click(function)(){
+		$('content tab-2').hide('a href')
+	});
+
+	$('.second-div').click(function(){
+		$(this).toggleClass("really-tall")
+	});
+	</script>
+<script>
+	$('.third-div').click(function(){
+		$(this).toggleClass("really-big")
+	});
+	</script>
