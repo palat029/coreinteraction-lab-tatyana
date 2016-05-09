e1a29090c2b162962109d8a553666dbc30eb67e7
palat029 <TatyanaPalacios@tatyanas-mbp.nyc.rr.com>
2016-05-08 20:24:47 -0400

    update

--- project3/main.js
+++ project3/main.js
@@ -1,79 +1,98 @@
 
-
 var username;
 
 $('.submit-button').click(function (e) {
-  username = $('.name-input').val();
-  $('.name-form').addClass('is-hidden');
+  username = $('.username-input').val();
+
+  $('.username-form').addClass('is-hidden');
   $('.shape-input').removeClass('is-hidden');
 });
 
 
-$('.option-0').click(function () { sendMessage(0) })
-$('.option-1').click(function () { sendMessage(1).show() })
-$('.option-2').click(function () { sendMessage(2) })
-$('.option-3').click(function () { sendMessage(3) })
-$('.option-4').click(function () { sendMessage(4) })
-$('.option-5').click(function () { sendMessage(5) })
-$('.option-6').click(function () { sendMessage(6) })
-$('.option-7').click(function () { sendMessage(7) })
-$('.option-8').click(function () { sendMessage(8) })
-$('.option-9').click(function () { sendMessage(9) })
-$('.option-10').click(function () { sendMessage(10) })
-$('.option-11').click(function () { sendMessage(11) })
-$('.option-12').click(function () { sendMessage(12) })
+$('.option-0').click(function () {
+  sendMessage(0);
+});
 
-function sendMessage(val) {
+$('.option-1').click(function () {
+  sendMessage(1);
+});
+
+$('.option-2').click(function () {
+  sendMessage(2);
+});
+
+$('.option-3').click(function () {
+  sendMessage(3);
+});
+
+$('.option-4').click(function () {
+  sendMessage(4);
+});
+
+$('.option-5').click(function () {
+  sendMessage(5);
+});
+
+$('.option-6').click(function () {
+  sendMessage(6);
+});
+
+$('.option-7').click(function () {
+  sendMessage(7);
+});
+
+$('.option-8').click(function () {
+  sendMessage(8);
+});
+
+
+function sendMessage(value) {
   Chat.sendMessage({
     sender: username,
     type: 'shape',
-    value: val
+    value: value
   });
 }
 
-var shapeMap = {
+var shapeValues = {
   0: 'circle',
-  1: 'mallCircle', 
-  2: 'square', 
-  3: 'rectangle', 
-  4: 'oval',
-  5: 'triangle',
-  6: 'smallTriangle',
-  7: 'trapazoid',
-  8: 'smalltrapazoid',
-  9: 'smallOval',
-  10: 'smallSquare',
-  11: 'smallCircle',
-  12: 'smallRectangle'
+  1: 'smallCirc',
+  2: 'square',
+  3: 'smallSqua',
+  4: 'rect',
+  5: 'option-5',
+  6: 'option-6',
+  7: 'option-7',
+  8: 'option-8',
+  9: 'option-9',
+  10: 'option-10',
+  11: 'option-11',
+  12: 'option-12'
 };
-// var colorMap = {
-//   5: 'border-color'
-//   6: 'border-color'
-//   7: 'border-color'
-//   8: 'border-color'
-// }
 
-Chat.onMessage(function (messageData) {
-
+Chat.onMessage(function (data) {
 
   var message = $('<div></div>');
   $(message).addClass('message');
-  $(message).html(messageData.sender);
-  
-  $(message).addClass('option-' + messageData.value);
-  var randHue= Math.random() * 360;
-  var randColor="hsl("+ randHue +",50%,50%)";
 
-  $(message).css("background-color", randColor );
+
+  $(message).addClass(shapeValues[data.value]);
+   $(message).addClass('option-'+ data.value);
+  var randHue= Math.random() * 300;
+  var randColor="hsl("+ randHue +",60%,60%)";
+
+  $(message).css("background-color", randColor);
+
  
+  var messageLabel = $('<div></div>');
+  // $(messageLabel).html(data.sender);
+
+
+  $(message).append(messageLabel);
+
+  $('.message-container').append(message);
 
   var elem = document.getElementById('message-container');
       elem.scrollTop = elem.scrollHeight;
-
-  // $(message).css('colorMap' + messageData.value, randColor);
-
-  if (messageData.sender == username) {
-    $(message).addClass('from-current-user');
-  }
-  $('.message-container').append(message);
 });
+
